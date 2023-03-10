2bb780b84d9a3bd802bada9df2d99c3577f733dd
Mary Ojo <maryojo3@gmail.com>
2023-03-10 10:10:17 +0100

    Stored data in local storage - remove later

--- src/pages/searchresult.js
+++ src/pages/searchresult.js
@@ -6,7 +6,7 @@ import Loader from "../components/Loader";
 import Error from "../components/Error";
 import Comments from "../components/Comments";
 import Sidebar from "../components/Sidebar";
-import DataAnalysis from "../components/DataAnalysis";
+// import DataAnalysis from "../components/DataAnalysis";
 // import { textAnalysis, keyPhraseExtraction } from "../components/SentimentAnalysis";
 
 // import { getSentimentScore } from '../model/sentimentAnalysis'
@@ -14,21 +14,46 @@ import DataAnalysis from "../components/DataAnalysis";
 const SearchResult = () => {
   const { searchTerm } = useParams();
   let tweetArr = [];
+  
+  let tweetData = {};
+  let tweetError = '';
+  let tweetLoading = false;
 
-  const [data, error, loading] = useAxios({
-    axiosInstance: axios,
-    method: "GET",
-    url: "https://twitter135.p.rapidapi.com/Search/",
-    requestConfig: {
-      params: {
-        q: searchTerm,
-        count: "20",
+  //stored data in localstorage for development 
+
+  if(localStorage.getItem('tweetArr')) {
+    [tweetData, tweetError, tweetLoading] = JSON.parse(localStorage.getItem('tweetArr'));
+  } else {
+    GetTweetData(searchTerm);
+  }
+
+ 
+  function GetTweetData(term){
+     [tweetData, tweetError, tweetLoading] = useAxios({
+
+      axiosInstance: axios,
+      method: "GET",
+      url: "https://twitter135.p.rapidapi.com/Search/",
+      requestConfig: {
+        params: {
+          q: term,
+          count: "20",
+        },
       },
-    },
-  });
+    });  
+  }
 
-  console.log(data);
+  //equate data to tweetInfo saved
+
+  let data = tweetData;
+  let error = tweetError;
+  let loading = tweetLoading;
+  
+
+  console.log( data);
   console.log(error);
+  console.log(loading);
+
 
   // const convertToSentence = (text) =>{
   //   text.split('\n').map((line) =>(
@@ -48,6 +73,9 @@ const SearchResult = () => {
 
   console.log(tweetArr);
 
+  localStorage.setItem("tweetArr", JSON.stringify([data, error, loading]));
+
+
   const documents = [
     "I did not like the restaurant. The food was too spicy.",
     "The restaurant was decorated beautifully. The atmosphere was unlike any other restaurant I've been to.",
@@ -61,36 +89,16 @@ const SearchResult = () => {
   return (
     <div className="p-16">
       <div className="flex justify-between">
-        <div className="flex flex-col">
-          <div className="flex flex-col">
-            <div>
-              Settings
-            </div>
-            <div>
-              <div className="flex gap-2">
-              <p>Language</p>
-              <dropdown>
-                <select>English</select>
-              </dropdown>
-              </div>
-              <div className="flex gap-2">
-              <p>Language</p>
-              <dropdown>
-                <select>English</select>
-              </dropdown>
-              </div>
-            </div>
-          </div>
-        </div>
+        
         <div>
           <div className="font-bold text-2xl mb-5">
-            Search results for {searchTerm}
+            {`Search results for "${searchTerm}"`}
           </div>
           
-          <div className="flex flex-col ">
+          <div className="flex justify-between">
            <Sidebar />
-           <Comments tweets={tweetArr}/>
-           <DataAnalysis />
+           <Comments tweets={tweetArr} />
+           {/* <DataAnalysis /> */}
           </div>
         </div>
         <div></div>
